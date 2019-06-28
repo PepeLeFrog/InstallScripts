27efdbf9ba400cede6dcf7c19cb1f3f3af8dc580
A Total New Guy <34976990+PepeLeFrog@users.noreply.github.com>
2019-06-27 23:34:55 -0500

    x
    x

---
+++ install_script.sh
@@ -0,0 +1,39 @@
+##  Copy this script to your computer somewhere
+##  Open up the directory where it sits in Thunar (for example)
+##  Right-Click on the Script
+##  go to >Properties then >Permissions
+##  Check the box where it says, "Allow this file to run as a program"
+##  X-Close
+##  Open Terminal where this script sits
+##  Type in ./install_script.sh
+##  Let it run and answer the questions
+
+sudo apt-get install zenmap -y
+sudo apt-get install xscreensaver -y 
+sudo apt-get install waterfox -y 
+sudo apt-get install w3m -y 
+sudo apt-get install tor -y 
+sudo apt-get install telnet -y 
+sudo apt-get install stegosuite -y  
+sudo apt-get install speedtest-cli -y  
+sudo apt-get install p7zip -y  
+sudo apt-get install mutt -y 
+sudo apt-get install mumble -y 
+sudo apt-get install kdenlive -y 
+sudo apt-get install hexchat -y 
+sudo apt-get install gimp -y  
+sudo apt-get install mousepad -y 
+sudo apt-get install featherpad -y 
+sudo apt-get install chirp -y 
+sudo apt-get install clementine -y 
+sudo apt-get install calibre -y  
+sudo apt-get install screenfetch -y 
+sudo apt-get install youtube-dl -y
+sudo apt-get install neofetch -y 
+sudo apt-get install ffmpeg -y 
+sudo apt-get install focuswriter -y
+sudo apt-get install ipfs-desktop -y
+
+##  Many of these items may already be installed on your computer, 
+##  But, if they are not, then, Here you go!
+
